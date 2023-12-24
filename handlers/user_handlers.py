c195a2e24fb48a0b2a26d018e30604b6c26a4cf8
Viktor Tumanian <victumanyan@gmail.com>
2023-12-24 20:10:19 +0400

    added secret command "delmenu"

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,5 +1,10 @@
-from aiogram import Router
+from aiogram import Bot, Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 
 router = Router()
+
+@router.message(Command(commands='delmenu'))
+async def del_main_menu(message: Message, bot: Bot):
+    await bot.delete_my_commands()
+    await message.answer(text='Кнопка "Menu" удалена')
